682f0ddb55ce3b798b803c94319414c3ccb566e6
salaahl <67805036+salaahl@users.noreply.github.com>
2023-08-19 15:44:57 +0000

    Mise à jour

--- catalog/static/js/search_book.js
+++ catalog/static/js/search_book.js
@@ -1,57 +1,78 @@
+let timer;
+
 document
-	.querySelector('.search-bar')
-	.addEventListener('input', (e) => {
-		e.preventDefault();
+.querySelector('.search-bar')
+.addEventListener('input', (e) => {
+    e.preventDefault();
 
-		let $ = (id) => {
-			return document.querySelector(id);
-		};
+    clearTimeout(timer);
 
-    $('.search-results').innerHTML = '';
-    $('.search-results').style.opacity = '0';
+    let $ = (id) => {
+        return document.querySelector(id);
+    };
 
-		let formData = new FormData();
-		formData.append('book', $("[name=search]").value);
+    let body = document.querySelectorAll("body .container-fluid > .row > div:last-child > .search-container ~ *");
 
-		let csrfTokenValue = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    timer = setTimeout(function() {
+        if ($('[name=search]').value.length > 1) {
+            body.forEach(content => {
+                content.style.filter = 'blur(5px) opacity(0)'
+            })
 
-		const request = new Request('/catalog/', {
-			method: 'POST',
-			body: formData,
-			headers: {
-				'X-CSRFToken': csrfTokenValue
-			}
-		});
-		fetch(request)
-			.then(response => response.json())
-			.then(result => {
-        if(result.books != '') {
-          $('.search-results').style.opacity = '1';
-          $('.search-results').innerHTML =
-          `<table class="table table-striped">
-            <thead>
-              <tr class="table-row">
-                <th scope="col">Titre</th>
-                <th scope="col">Auteur</th>
-              </tr>
-            </thead>
-            <tbody class="table-body">
-            </tbody>
-          </table>`
-        
-          result.books.forEach(book => {
-            $('.table-body').innerHTML +=
-              '<tr class="table-row">' +
-                '<td><a href="/catalog/book/' + book['id'] + '">' + book['title'] + '</a></td>' +
-                '<td><a href="/catalog/author/' + book['author_id'] + '">' + book['author'] + '</a></td>' +
-              '</tr>';
-          })
+            $('.search-results').innerHTML = '';
+            $('.search-results').style.opacity = '0';
+
+            let formData = new FormData();
+            formData.append('book', $("[name=search]").value);
+
+            let csrfTokenValue = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+            const request = new Request('/catalog/', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'X-CSRFToken': csrfTokenValue
+                }
+            });
+            fetch(request)
+                .then(response => response.json())
+                .then(result => {
+                  $('.search-results').style.opacity = '1';
+                    if (result.books != '') {
+                        $('.search-results').innerHTML =
+                            `<table class="table table-striped">
+                            <thead>
+                              <tr class="table-row">
+                                <th scope="col">Titre</th>
+                                <th scope="col">Auteur</th>
+                              </tr>
+                            </thead>
+                            <tbody class="table-body">
+                            </tbody>
+                          </table>`
+
+                        result.books.forEach(book => {
+                            $('.table-body').innerHTML +=
+                                '<tr class="table-row">' +
+                                '<td><a href="/catalog/book/' + book['id'] + '">' + book['title'] + '</a></td>' +
+                                '<td><a href="/catalog/author/' + book['author_id'] + '">' + book['author'] + 
+                              '</a></td>' + '</tr>';
+                        })
+                    } else {
+                      $('.search-results').innerHTML = 'Aucun résultat';
+                    }
+                })
+                .catch((error) => {
+                    console.log(error.message)
+                    alert(
+                        'Erreur.'
+                    );
+                });
+        } else {
+            body.forEach(content => {
+                content.style.filter = 'blur(0px) opacity(1)'
+            })
+            $('.search-results').innerHTML = '';
         }
-			})
-			.catch((error) => {
-				console.log(error.message)
-				alert(
-					'Erreur.'
-				);
-			});
-	});
+    }, 600);
+});
