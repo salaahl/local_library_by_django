e74e7c7b36d8b7a628eba99a01975e47e3985b52
salaahl <sokhona.salaha@gmail.com>
2023-08-22 21:45:10 +0200

    ajustement

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,11 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import sys
+from dotenv import load_dotenv
 
-if sys.argv[1] == 'runserver':
-    from dotenv import load_dotenv
-    load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
