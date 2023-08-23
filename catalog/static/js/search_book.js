4bff2f417610b93e3f1abd059bb73392d940b702
salaahl <67805036+salaahl@users.noreply.github.com>
2023-08-23 12:43:10 +0200

    Update search_book.js

--- catalog/static/js/search_book.js
+++ catalog/static/js/search_book.js
@@ -1,86 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let timer;
-    
-    document
-    .querySelector('.search-bar')
-    .addEventListener('input', (e) => {
-        e.preventDefault();
-    
-        clearTimeout(timer);
-    
-        let $ = (id) => {
-            return document.querySelector(id);
-        };
-    
-        let body = document.querySelectorAll("#page-container > #search-container ~ *");
-        let cont = document.querySelectorAll("#page-container > #search-container ~ *");
-        console.log(cont)
-        let connt = document.querySelectorAll("#search-container ~ *");
-        console.log(connt)
-        let connet = document.querySelectorAll("#search-container");
-        console.log(connet)
-    
-        timer = setTimeout(function() {
-            if ($('[name=search]').value.length > 1) {
-                body.forEach(content => {
-                    content.style.filter = 'blur(5px) opacity(0)';
-                })
-    
-                $('#search-results').innerHTML = '';
-                $('#search-results').style.opacity = '0';
-    
-                let formData = new FormData();
-                formData.append('book', $("[name=search]").value);
-    
-                let csrfTokenValue = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    
-                const request = new Request('/catalog/', {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'X-CSRFToken': csrfTokenValue
+let timer;
+
+document
+.querySelector('.search-bar')
+.addEventListener('input', (e) => {
+    e.preventDefault();
+
+    clearTimeout(timer);
+
+    let $ = (id) => {
+        return document.querySelector(id);
+    };
+
+    let body = document.querySelectorAll("#search-container ~ *");
+        
+    timer = setTimeout(function() {
+        if ($('[name=search]').value.length > 1) {
+            body.forEach(content => {
+                content.style.filter = 'blur(5px) opacity(0)';
+            })
+
+            $('#search-results').innerHTML = '';
+            $('#search-results').style.opacity = '0';
+
+            let formData = new FormData();
+            formData.append('book', $("[name=search]").value);
+
+            let csrfTokenValue = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+            const request = new Request('/catalog/', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'X-CSRFToken': csrfTokenValue
+                }
+            });
+            fetch(request)
+                .then(response => response.json())
+                .then(result => {
+                  $('#search-results').style.opacity = '1';
+                    if (result.books != '') {
+                        $('#search-results').innerHTML =
+                            `<table class="table table-striped">
+                            <thead>
+                              <tr class="table-row">
+                                <th scope="col">Titre</th>
+                                <th scope="col">Auteur</th>
+                              </tr>
+                            </thead>
+                            <tbody class="table-body">
+                            </tbody>
+                          </table>`
+
+                        result.books.forEach(book => {
+                            $('tbody').innerHTML +=
+                                '<tr class="table-row">' +
+                                '<td><a href="/catalog/book/' + book['id'] + '">' + book['title'] + '</a></td>' +
+                                '<td><a href="/catalog/author/' + book['author_id'] + '">' + book['author'] + 
+                              '</a></td>' + '</tr>';
+                        })
+                    } else {
+                      $('#search-results').innerHTML = 'Aucun résultat';
                     }
-                });
-                fetch(request)
-                    .then(response => response.json())
-                    .then(result => {
-                      $('#search-results').style.opacity = '1';
-                        if (result.books != '') {
-                            $('#search-results').innerHTML =
-                                `<table class="table table-striped">
-                                <thead>
-                                  <tr class="table-row">
-                                    <th scope="col">Titre</th>
-                                    <th scope="col">Auteur</th>
-                                  </tr>
-                                </thead>
-                                <tbody class="table-body">
-                                </tbody>
-                              </table>`
-    
-                            result.books.forEach(book => {
-                                $('tbody').innerHTML +=
-                                    '<tr class="table-row">' +
-                                    '<td><a href="/catalog/book/' + book['id'] + '">' + book['title'] + '</a></td>' +
-                                    '<td><a href="/catalog/author/' + book['author_id'] + '">' + book['author'] + 
-                                  '</a></td>' + '</tr>';
-                            })
-                        } else {
-                          $('#search-results').innerHTML = 'Aucun résultat';
-                        }
-                    })
-                    .catch((error) => {
-                        console.log(error.message)
-                        alert(
-                            'Erreur.'
-                        );
-                    });
-            } else {
-                body.forEach(content => {
-                    content.style.filter = 'blur(0px) opacity(1)';
                 })
-                $('#search-results').innerHTML = '';
-            }
-        }, 600);
-    });
+                .catch((error) => {
+                    console.log(error.message)
+                    alert(
+                        'Erreur.'
+                    );
+                });
+        } else {
+            body.forEach(content => {
+                content.style.filter = 'blur(0px) opacity(1)';
+            })
+            $('#search-results').innerHTML = '';
+        }
+    }, 600);
 });
