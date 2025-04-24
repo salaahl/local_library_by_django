14e4784a0e9d63b56bf13ca813f05cbf87584a35
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2025-04-24 11:29:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,16 @@ echo "✅ Base de données accessible"
 echo "🛠️  Migration de la base de données..."
 python manage.py migrate --noinput
 
+# Créer un superutilisateur s’il n’existe pas déjà
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+username = '${SUPERUSER_USERNAME}'; \
+email = '${SUPERUSER_EMAIL}'; \
+password = '${SUPERUSER_PASSWORD}'; \
+User.objects.filter(username=username).exists() or \
+User.objects.create_superuser(username=username, email=email, password=password)" \
+| python manage.py shell
+
 echo "📦 Collecte des fichiers statiques..."
 python manage.py collectstatic --noinput
 
